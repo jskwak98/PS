57f40f8a15e65dc86b609b2c38cb97b7236ed5bc
jskwak98 <jskwak98@naver.com>
2021-09-13 02:42:46 +0900

    boj 18428 solved

---
+++ BOJ/BruteForce/18428.py
@@ -0,0 +1,68 @@
+from itertools import combinations
+from copy import deepcopy
+
+
+def check(teachers, obstacles):
+    ts = deepcopy(teachers)
+    for obstacle in obstacles:
+        for teacher in teachers:
+            for student in teachers[teacher]:
+                if teacher[0] == student[0] == obstacle[0] and min(teacher[1], student[1]) < obstacle[1] < max(teacher[1], student[1]):
+                    if student in ts[teacher]:
+                        ts[teacher].remove(student)
+                elif teacher[1] == student[1] == obstacle[1] and min(teacher[0], student[0]) < obstacle[0] < max(teacher[0], student[0]):
+                    if student in ts[teacher]:
+                        ts[teacher].remove(student)
+    for teacher in ts:
+        if len(ts[teacher]) != 0:
+            return False
+    return True
+
+
+def solution():
+    n = int(input())
+    board = []
+    teachers = dict()
+    students = set()
+    candidates = []
+    for row in range(n):
+        board.append(input().split())
+        for col in range(n):
+            if board[row][col] == 'T':
+                teachers[(row, col)] = set()
+            elif board[row][col] == 'S':
+                students.add((row, col))
+    for teacher in teachers:
+        for student in students:
+            if teacher[0] == student[0]:
+                if abs(teacher[1] - student[1]) == 1:  # no space exists between S and T
+                    print("NO")
+                    return
+                else:
+                    teachers[teacher].add(student)
+                    for col in range(min(teacher[1], student[1])+1, max(teacher[1], student[1])):
+                        candidate = (teacher[0], col)
+                        if candidate not in students and candidate not in teachers and candidate not in candidates:
+                            candidates.append(candidate)
+            elif teacher[1] == student[1]:
+                if abs(teacher[0] - student[0]) == 1:  # no space exists between S and T
+                    print("NO")
+                    return
+                else:
+                    teachers[teacher].add(student)
+                    for row in range(min(teacher[0], student[0])+1, max(teacher[0], student[0])):
+                        candidate = (row, teacher[1])
+                        if candidate not in students and candidate not in teachers and candidate not in candidates:
+                            candidates.append(candidate)
+    if len(candidates) <= 3:
+        print("YES")
+        return
+    for obstacles in combinations(candidates, 3):
+        if check(teachers, obstacles):
+            print("YES")
+            return
+    print("NO")
+    return
+
+
+solution()
